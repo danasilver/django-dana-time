5bc2189beb15e79869f1dead38308859681525bb
danasilver <dsilver1221@gmail.com>
2013-08-10 01:48:47 -0400

    remove django timesince and add support for dates past 1 day ago

--- dana-time.py
+++ dana-time.py
@@ -15,23 +15,48 @@ def danatime(value):
 
   now = datetime.now(utc if is_aware(value) else None)
 
+  is_today = False
+  if value.date() == datetime.today().date():
+    is_today = True
+
+  is_this_year = False
+  if value.date().year = datetime.today().year
+    is_this_year = True
+
   if value < now:
     # Check that value is in the past
     delta = now - value
-    if delta.days != 0:
-      # use Django timesince if days > 0
-      return pgettext('gmailtime', '%(delta)s') 
-        % {'delta': defaultfilters.timesince(value, now)}
     elif delta.seconds == 0:
+      # Now
       return _('now')
     elif delta.seconds < 60:
-      return ungettext('a second', '%(count)s\u00a0seconds', delta.seconds)
-        % {'count': delta.seconds}
-    elif delta.seconds // 60 < 60
+      # One or multiple seconds
+      # \u00a0 is a nonbreaking space
+      return ungettext(
+        'a second ago', '%(count)s\u00a0seconds ago', delta.seconds
+        ) % {'count': delta.seconds}
+    elif delta.seconds // 60 < 60:
+      # One or multiple minutes
       count = delta.seconds // 60
-      return ungettext('a minute', '%(count)s\u00a0minutes', count)
-        % {'count': count}
+      return ungettext(
+        'a minute ago', '%(count)s\u00a0minutes ago', count
+        ) % {'count': count}
+    elif is_today:
+      # Same day
+      if delta.seconds // (60 * 60) <= 6:
+        # Up to 6 hours ago on the same day
+        count = delta.seconds // (60 * 60)
+        return ungettext(
+          'an hour ago', '%(count)s\u00a0hours ago', count
+          ) % {'count': count}
+      else:
+        # Up to 24 hours ago on the same day
+        return _("%(hour)s:%(minute)s %p") % {'hour': value.time().hour, 'minute': value.time().minute}
+    elif is_this_year:
+      return _(value.strftime("%d %b").lstrip('0'))
     else:
-      count = delta.seconds // 60 // 60
-      return ungettext('an hour', '%(count)s\u00a0hours ago', count)
-        % {'count': count}
+      return _(value.strftime(
+        "%(day)d/%(month)d/%(year)d"
+        ) % {'day': value.date().day, 
+        'month': value.date().month, 
+        'year': value.date().year})
